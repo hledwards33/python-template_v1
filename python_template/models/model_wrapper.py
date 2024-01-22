e4d86e6daf8ca9e972c6e6e25985fcadeced1e8f
Harry Edwards <H.Edwards.2@outlook.com>
2024-01-22 15:22:05 +0000

    starting to create the model wrapper

--- python_template/models/model_wrapper.py
+++ python_template/models/model_wrapper.py
@@ -1,2 +1,19 @@
+import pandas as pd
+
+
 class ModelWrapper:
-    pass
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def data_pre_processing(data: dict) -> dict:
+        pass
+
+    @staticmethod
+    def set_parameters_from_dataframe(data: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    @staticmethod
+    def data_post_processing(data: dict) -> dict:
+        pass
