ccbe5fb5ef2538dbe25aa40f7434b97bcb3178ad
Harry Edwards <H.Edwards.2@outlook.com>
2024-04-30 13:09:28 +0100

    starting framework for outputting data

--- src/framework/data_scripts/read_inputs.py
+++ src/framework/data_scripts/read_inputs.py
@@ -10,7 +10,21 @@ class InputData:
         pass
 
     @staticmethod
-    def read_csv(path: str, schema: dict) -> pd.DataFrame:
+    def read_json(path: str) -> dict:
+        with open(path, 'r') as file:
+            result = json.load(file)
+
+        return result
+
+    @staticmethod
+    def read_yaml(path: str) -> dict:
+        with open(path, 'r') as file:
+            result = yaml.safe_load(file)
+
+        return result
+
+    @staticmethod
+    def read_csv_to_pandas(path: str, schema: dict) -> pd.DataFrame:
         # TODO: Log dataset x is being read in, only columns defined in schemas are read
         # TODO: Check if this works with zip files and add if_zip to the read data func
         kwargs = {'path': path, 'dtype': schema, 'usecols': schema.keys(), 'cache_dates': True,
@@ -23,20 +37,25 @@ class InputData:
         return data
 
     @staticmethod
-    def read_parquet(path: str, schema: dict) -> pd.DataFrame:
+    def read_parquet_to_pandas(path: str, schema: dict) -> pd.DataFrame:
         # TODO: Fill in this method
         pass
 
     @staticmethod
-    def read_json(path: str) -> dict:
-        with open(path, 'r') as file:
-            result = json.load(file)
-
-        return result
+    def read_csv_to_spark(path: str, schema: dict) -> pd.DataFrame:
+        # TODO: Fill in this method
+        pass
 
     @staticmethod
-    def read_yaml(path: str) -> dict:
-        with open(path, 'r') as file:
-            result = yaml.load(file)
+    def read_parquet_to_spark(path: str, schema: dict) -> pd.DataFrame:
+        # TODO: Fill in this method
+        pass
 
-        return result
+    @staticmethod
+    def write_csv_from_pandas(data: pd.DataFrame, path: str, schema: dict):
+        pass
+
+    @staticmethod
+    def write_parquet_from_pandas(data: pd.DataFrame, path: str, schema: dict):
+        # TODO: Fill in this method
+        pass
--- src/framework/model_wrapper.py
+++ src/framework/model_wrapper.py
@@ -1,7 +1,5 @@
 from abc import ABC, abstractmethod
 
-import yaml
-
 from framework.data_scripts.read_inputs import InputData
 
 
@@ -11,32 +9,18 @@ class ModelWrapper(ABC):
         pass
 
     @staticmethod
-    def read_config(path: str) -> dict:
-        config_dict = InputData.read_yaml(path=path)
-
-        return config_dict
-
-    @staticmethod
     def set_parameters():
         pass
 
     @staticmethod
-    def data_pre_processing(data: dict) -> dict:
-        pass
-
-    @staticmethod
-    def data_post_processing(data: dict) -> dict:
-        pass
-
-    def read_schemas(self):
-        schema_dict = self.define_input_schemas()
+    def read_schemas(schema_dict: dict) -> dict:
 
         schema_dict = {key: InputData.read_json(path=val) for key, val in schema_dict.items()}
 
         return schema_dict
 
     @staticmethod
-    def read_data(model_config: dict, file_schemas: dict) -> dict:
+    def read_data_to_pandas(model_config: dict, file_schemas: dict) -> dict:
 
         data_dict = {}
 
@@ -46,39 +30,106 @@ class ModelWrapper(ABC):
 
             if file_type in ["csv", "zip"]:
 
-                data_dict[key] = InputData.read_csv(path=val, schema=file_schemas[key])
+                data_dict[key] = InputData.read_csv_to_pandas(path=val, schema=file_schemas[key])
 
             elif file_type in ["pqt", "parquet"]:
 
-                data_dict[key] = InputData.read_parquet(path=val, schema=file_schemas[key])
+                data_dict[key] = InputData.read_parquet_to_pandas(path=val, schema=file_schemas[key])
 
         return data_dict
 
     @staticmethod
-    def write_data(path: str, file_schemas: dict) -> dict:
+    def read_data_to_spark(model_config: dict, file_schemas: dict) -> dict:
+        # TODO: fill in the method
+        pass
+
+    @staticmethod
+    def write_data_from_pandas(model_config: dict, file_schemas: dict):
+
+        for key, val in model_config['inputs']:
+
+            file_type = val.split(".")[-1]
+
+            if file_type in ["csv", "zip"]:
+
+                InputData.write_csv(path=val, schema=file_schemas[key])
+
+            elif file_type in ["pqt", "parquet"]:
+
+                InputData.write_parquet(path=val, schema=file_schemas[key])
+
+        return data_dict
+
+    @staticmethod
+    def write_data_from_spark(path: str, file_schemas: dict):
+        # TODO: Fill in this method
+        pass
+
+    @staticmethod
+    def data_pre_processing() -> dict:
+        pass
+
+    @staticmethod
+    def data_post_processing() -> dict:
         pass
 
     @abstractmethod
-    def define_input_schemas(self):
+    def define_input_schemas(self) -> dict:
         pass
 
     @abstractmethod
-    def define_output_schemas(self):
+    def define_output_schemas(self) -> dict:
+        pass
+
+    @abstractmethod
+    def run_model(self) -> dict:
         pass
 
 
 class DeployWrapper:
 
-    def __init__(self):
+    def __init__(self, model_wrapper: ModelWrapper, sys_config: str, model_config: str):
+        self.model_wrapper = model_wrapper
+        self.sys_config = self.read_config(sys_config)
+        self.model_config = self.read_config(model_config)
+
+    def run_model(self):
+        input_data = self.get_inputs()
+
+        output_data = self.run_model(input_data)
+
+        self.post_outputs(data=output_data)
+
+    def post_outputs(self, data: dict):
+        if self.sys_config['model_parameters']['type'] == "pandas":
+            output_schemas = self.model_wrapper.read_schemas(schema_dict=self.model_wrapper.define_input_schemas())
+
+        elif self.sys_config['model_parameters']['type'] == "pyspark":
+            input_schemas = self.model_wrapper.read_schemas(schema_dict=self.model_wrapper.define_input_schemas())
+
+    def get_inputs(self) -> dict:
+        if self.sys_config['model_parameters']['type'] == "pandas":
+            input_schemas = self.model_wrapper.read_schemas(schema_dict=self.model_wrapper.define_input_schemas())
+
+            input_data = self.model_wrapper.read_data_to_pandas(model_config=self.model_config,
+                                                                file_schemas=input_schemas)
+
+        elif self.sys_config['model_parameters']['type'] == "pyspark":
+            input_schemas = self.model_wrapper.read_schemas(schema_dict=self.model_wrapper.define_input_schemas())
+
+            input_data = self.model_wrapper.read_data_to_pandas(model_config=self.model_config,
+                                                                file_schemas=input_schemas)
+
+        else:
+            raise ImportError('Parameter "Type" is defined incorrectly. Type can take values ["pandas", "pyspark"].')
+
+        return input_data
+
+    def create_data_dict(self):
         pass
 
-    def run_model(self, model_wrapper: ModelWrapper, sys_config: str, model_config: str):
-        sys_config = self.read_config(sys_config)
-        model_config = self.read_config(model_config)
-
     @staticmethod
-    def read_config(config_path: str) -> dict:
-        with open(config_path, 'r') as file:
-            config_dict = yaml.safe_load(file)
+    def read_config(path: str) -> dict:
+        config_dict = InputData.read_yaml(path=path)
 
         return config_dict
