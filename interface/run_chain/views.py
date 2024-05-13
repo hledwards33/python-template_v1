7cf80eeee2a83aef55eea27664f4a7b2daca36f7
Harry Edwards <H.Edwards.2@outlook.com>
2024-05-13 20:19:04 +0100

    added changes to run chain app

--- interface/run_chain/views.py
+++ interface/run_chain/views.py
@@ -1,19 +1,23 @@
 import os
+from glob import glob
 
 from django.shortcuts import render
 
 PY_FILE_DIR = os.path.abspath(os.path.dirname(__file__))
 PY_ROOT_DIR = os.path.abspath(os.path.join(PY_FILE_DIR, ".."))
 PY_REPO_DIR = os.path.dirname(PY_ROOT_DIR)
-WRAPPER_DIR = r"src/models/model_wrappers"
+CONFIG_DIR = r"config"
 
 
 def index(request):
-    path = os.path.join(PY_REPO_DIR, WRAPPER_DIR)
-    wrappers = [file for file in os.listdir(path) if ((file[:1] != "_") & ("chain" in file))]
+    path = os.path.join(PY_REPO_DIR, CONFIG_DIR)
+    configs = [os.path.split(file)[-1]
+               for p, subdir, files in os.walk(path)
+               for file in glob(os.path.join(p, "*.yml"))]
+    configs = [config for config in configs if ("chain" in config) & (config[:1] != "_")]
 
-    return render(request, "run_chain/index.html", {'wrappers': wrappers})
+    return render(request, "run_chain/index.html", {'configs': configs})
 
 
 def run(request, model_id):
-    return render(request, "run_model/run.html", context={'model_id': model_id})
+    return render(request, "run_chain/run.html", context={'model_id': model_id})
