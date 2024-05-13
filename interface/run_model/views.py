c8848b1c214d6b2942a9f69dc10d6228e7138653
Harry Edwards <H.Edwards.2@outlook.com>
2024-05-13 22:44:34 +0100

    both model run and model chain now work

--- interface/run_model/views.py
+++ interface/run_model/views.py
@@ -1,8 +1,12 @@
+import datetime
+import importlib
 import os
 import sys
 from glob import glob
 
+import yaml
 from django.shortcuts import render
+from django.contrib import messages
 
 PY_FILE_DIR = os.path.abspath(os.path.dirname(__file__))
 PY_ROOT_DIR = os.path.abspath(os.path.join(PY_FILE_DIR, ".."))
@@ -10,6 +14,7 @@ PY_REPO_DIR = os.path.dirname(PY_ROOT_DIR)
 CONFIG_DIR = r"config/model_config/model_metadata"
 
 sys.path.append(os.path.join(PY_REPO_DIR, "src"))
+sys.path.append(PY_REPO_DIR)
 from framework.model_wrapper import DeployWrapper
 
 
@@ -28,12 +33,32 @@ def run(request, model_id):
             for p, subdir, files in os.walk(path)
             for file in glob(os.path.join(p, "*.yml")) if model_id == os.path.split(file)[-1][:-4]][0]
 
-    model = DeployWrapper
+    run_model(request, path)
 
-    return render(request, "run_chain/run.html", context={'model_id': model_id})
+    return render(request, "run_model/run.html", context={'model_id': model_id})
 
 
-def run_chain(path, request):
-    model_chain = ModelChain(path)
+def run_model(request, path: str):
+    with open(path, 'r') as file:
+        config = yaml.safe_load(file)['config']
 
-    model_chain.run_chain()
+    DeployWrapper(get_model_class(config), config['sys_config'],
+                  config['model_config']).run_model()
+
+    with open(os.path.join(PY_REPO_DIR, config['model_config'])) as file:
+        config = yaml.safe_load(file)['parameters']['model_parameters']
+
+    log_name = config['log_name']
+    if '{date}' in log_name:
+        log_name = log_name.format(date=datetime.date.today())
+
+    with open(os.path.join(PY_REPO_DIR, config['log_location'], log_name) + ".log", 'r') as file:
+        logs = file.read().splitlines()
+
+    for line in logs:
+        messages.add_message(request, messages.INFO, line)
+
+
+def get_model_class(model_config: dict):
+    model_class = getattr(importlib.import_module(model_config['model_path']), model_config['model'])
+    return model_class
