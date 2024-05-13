c8d45fe0483f099f2980868656b663bd841cca3d
Harry Edwards <H.Edwards.2@outlook.com>
2024-05-13 10:23:43 +0100

    working on model reconciliation template

--- tests/data_reconciliation/run_reconciliation.py
+++ tests/data_reconciliation/run_reconciliation.py
@@ -3,7 +3,7 @@ from tests.data_reconciliation.framework.reconciliation_full_analysis import run
 if __name__ == "__main__":
     kwargs = {
         'sys_config_path': r"config/system_config.yml",
-        'recon_config_path': r"config/model_config/example_model/example_model_reconciliation.yml"
+        'recon_config_path': r"config/model_config/example_model/example_model_reconciliation_config.yml"
     }
 
     run_full_data_reconciliation(**kwargs)
