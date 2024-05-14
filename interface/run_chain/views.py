522554de92e2b5148ee4342d29e3e35bf449cacc
Harry Edwards <H.Edwards.2@outlook.com>
2024-05-14 04:10:26 +0100

    Need to add read me to django and merge

--- interface/run_chain/views.py
+++ interface/run_chain/views.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import sys
 from glob import glob
@@ -38,12 +39,33 @@ def run(request, model_id):
             for p, subdir, files in os.walk(path)
             for file in glob(os.path.join(p, "*.yml")) if model_id == os.path.split(file)[-1][:-4]][0]
 
-    run_chain(path, request)
+    model_logs = run_chain(path, request)
 
-    return render(request, "run_chain/run.html", context={'model_id': model_id})
+    return render(request, "run_chain/run.html",
+                  context={'model_id': model_id, 'model_logs': model_logs})
 
 
 def run_chain(path, request):
     model_chain = ModelChain(path)
 
     model_chain.run_chain()
+
+    # Get model logs for each model in the chain
+    with open(path, 'r') as file:
+        config = yaml.safe_load(file)['models']
+
+    model_logs = {}
+    for key, val in config.items():
+        with open(os.path.join(PY_REPO_DIR, val['config']), 'r') as file:
+            log_info = yaml.safe_load(file)['parameters']['model_parameters']
+
+        log_name = log_info['log_name']
+        if '{date}' in log_name:
+            log_name = log_name.format(date=datetime.date.today())
+
+        with open(os.path.join(PY_REPO_DIR, log_info['log_location'], log_name) + ".log", 'r') as file:
+            logs = file.read().splitlines()
+
+        model_logs[key] = logs
+
+    return model_logs
