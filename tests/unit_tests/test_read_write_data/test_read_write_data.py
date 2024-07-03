acc0f120092a4ea24a1bf6ebd719bf607f596cd7
Harry Edwards <H.Edwards.2@outlook.com>
2024-07-03 15:41:54 +0100

    started writing tests for read_write_data.py

--- tests/unit_tests/test_read_write_data/test_read_write_data.py
+++ tests/unit_tests/test_read_write_data/test_read_write_data.py
@@ -1,11 +1,38 @@
 from src.framework.setup.read_write_data import *
 
 
-def test_read_json():
-    test_json_path = r"C:\Users\GV147BE\PycharmProjects\python-template\tests\unit_tests\test_read_write_data\test_data"
-    test_json_file = r"test_json.json"
-    result = read_json((test_json_path, test_json_file))
+def test_read_json_1():
+    # Arrange
+    from tests.unit_tests.test_read_write_data import test_data
+    test_json_path = (test_data, "test_json.json")
 
+    # Act
+    result = read_json(test_json_path)
+
+    # Assert
     expected = {"first_key": 1, "second_key": "two"}
+    assert result == expected
 
+
+def test_read_json_abs_1():
+    # Arrange
+    test_json_path = r"C:\Users\GV147BE\PycharmProjects\python-template\tests\unit_tests\test_read_write_data\test_data\test_json.json"
+
+    # Act
+    result = read_json_abs(test_json_path)
+
+    # Assert
+    expected = {"first_key": 1, "second_key": "two"}
+    assert result == expected
+
+
+def test_read_yaml_1():
+    # Arrange
+    test_yaml_path = r"C:\Users\GV147BE\PycharmProjects\python-template\tests\unit_tests\test_read_write_data\test_data\test_yaml.yaml"
+
+    # Act
+    result = read_yaml(test_yaml_path)
+
+    # Assert
+    expected = {"header": {"first_key": 1, "second_key": "two"}}
     assert result == expected
