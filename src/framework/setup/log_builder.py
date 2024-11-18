fd3d35e4e12798fce66737b405829c9394a72e80
Harry Edwards <H.Edwards.2@outlook.com>
2024-11-18 22:04:04 +0000

    added getters and setters

--- src/framework/setup/log_builder.py
+++ src/framework/setup/log_builder.py
@@ -1,10 +1,14 @@
+import logging
+import math
+import os
 import threading
+from datetime import datetime
 
-from log_handlers import ILogHandler, IFileHandler, ISysHandler, SysHandlerSimple
+from log_handlers import IFileHandler, ISysHandler, SysHandlerSimple
 
 
 class SingletonMeta(type):
-    """This is an eager implementation of the Singleton Pattern"""
+    """This is a lazy loading implementation of the Singleton Pattern"""
     _instances: dict = {}
     _lock: threading.Lock = threading.Lock()
 
@@ -21,9 +25,63 @@ class LogBuilder(metaclass=SingletonMeta):
         self._sys_handler = SysHandler
         self._file_handler = FileHandler
 
-    def add_handler(self, Handler: ILogHandler):
-        if isinstance(Handler, IFileHandler):
-            self._file_handler = Handler
+    @property
+    def file_handler(self):
+        return self._file_handler
 
-        elif isinstance(Handler, ISysHandler):
-            self._sys_handler = Handler
+    @property
+    def sys_handler(self):
+        return self._sys_handler
+
+    @file_handler.setter
+    def file_handler(self, file_handler: IFileHandler):
+        self._file_handler = file_handler
+
+    @sys_handler.setter
+    def sys_handler(self, sys_handler: ISysHandler):
+        self._sys_handler = sys_handler
+
+    def create_logging_file(self, file_handler, path: str, name: str, data_name: str = None) -> None:
+        format_dict = {}
+        if '{date}' in name:
+            format_dict['date'] = datetime.date.today()
+
+        if '{data}' in name:
+            format_dict['data'] = data_name
+
+        if format_dict:
+            name = name.format(**format_dict)
+
+        if sum([1 for handler in logging.getLogger().handlers if name in str(handler)]) < 1:
+            fil_handler = file_handler(os.path.join(path, name) + ".log")
+            logging.getLogger().addHandler(fil_handler)
+
+    def headers(self, message: str) -> None:
+        line_length = 75
+        line_num_start = line_length - math.ceil(len(message) / 2)
+        line_num_end = line_num_start if len(message) % 2 == 0 else line_num_start + 1
+        lines_start = ''.join(['-' for _ in range(line_num_start)])
+        lines_end = ''.join(['-' for _ in range(line_num_end)])
+        print()
+        print(lines_start + ' ' + message + ' ' + lines_end)
+        print()
+
+        simple_file_format()
+        logging.info("", extra={'block': ['console']})
+        logging.info(lines_start + ' ' + message + ' ' + lines_end + "\n", extra={'block': ['console']})
+        detailed_file_format()
+
+    def lines(self) -> None:
+        output = "".join(["-" for _ in range(152)])
+
+        print(output)
+
+        simple_file_format()
+        logging.info(output, extra={'block': ['console']})
+        detailed_file_format()
+
+    def no_format(message: str) -> None:
+        simple_file_format()
+        logging.info(message, extra={'block': ['console']})
+        print(message)
+        detailed_file_format()
